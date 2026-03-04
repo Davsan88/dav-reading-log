5ff8781e0be62f4b75252faf469baa96469ec24a
Dav <david.sanchez.martinez@outlook.com>
2026-03-04 14:23:09 +0100

    chore: scaffold home page with header, main and footer

---
+++ data.js
@@ -0,0 +1,128 @@
+export const booksArray = [
+    {
+        id: "title-slug",
+        title: " ",
+        author: " ",
+        coverImage: " ",
+        dateRead: " ",
+        entryDate: " ",
+        excerpt: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+    {
+        id: 0,
+        title: " ",
+        author: " ",
+        coverImg: " ",
+        dateRead: " ",
+        entryDate: " ",
+        content: `
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aenean eleifend lorem a ligula ornare tristique.Vestibulum nec efficitur enim.Proin non finibus quam.Proin dignissim finibus lacus quis ultricies.Proin quis massa sapien.Duis sed tortor vel sem consectetur scelerisque in ut orci.Maecenas porttitor porttitor ipsum vel commodo.Morbi semper fringilla sapien.Vestibulum dapibus arcu nisl, eu faucibus erat finibus ut.Integer a dignissim ligula, ac sagittis ligula.Nam mattis lectus pharetra venenatis vulputate.Mauris aliquet magna id ultricies porttitor.
+
+            Sed luctus diam nec tortor efficitur varius.Donec sit amet risus fringilla, hendrerit tellus euismod, placerat nunc.Donec nisi nunc, luctus nec eros at, sollicitudin rutrum dui.Sed pharetra tristique orci, vitae ullamcorper metus blandit in.Integer lacus massa, ultrices vitae ex id, aliquet ullamcorper tellus.Maecenas eget convallis ipsum.Integer nisi risus, condimentum ut augue nec, vulputate bibendum leo.
+            `,
+        tags: [" ", " "],
+        featured: true
+    },
+]
+
+
+
+
+
