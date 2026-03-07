3591c04236a74a51fe4f078799c2534b87e5e90f
Dav <david.sanchez.martinez@outlook.com>
2026-03-07 20:14:51 +0100

    refactor: simplify recent posts selection with filter, sort, and slice

--- main.js
+++ main.js
@@ -9,14 +9,13 @@ console.log(recentPostContainer)
 
 
 const generateRecentPostsCards = () => {
+    const recentPosts = posts
+        .filter(post => !post.featured)
+        .sort((a,b) => new Date(b.entryDate) - new Date(a.entryDate))
+        .slice(0, 3)
     
-    const unfeaturedPosts = posts.slice(0, -1)
-    console.log(unfeaturedPosts)
-    
-    const lastThreePost = unfeaturedPosts.slice(-3).sort((a, b) => new Date(b.entryDate) - new Date(a.entryDate))
-    console.log(lastThreePost)
 
-    const recentPostHtml = lastThreePost.map(post => {
+    const recentPostHtml = recentPosts.map(post => {
         return `<article>
                     <a href="">
                         <img src="${post.coverImage}" alt="" />
