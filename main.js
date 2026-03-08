4fda7bcc3d22bf9e1ce7c69774c7c93235acf4f1
Dav <david.sanchez.martinez@outlook.com>
2026-03-08 14:28:29 +0100

    feat: render featured post with background cover image

--- main.js
+++ main.js
@@ -1,36 +1,58 @@
 import { posts } from "./data.js"
 
-
+const featuredPostContainer = document.getElementById('featured-post-container')
 const recentPostContainer = document.getElementById('recent-post-container')
 
-console.log(recentPostContainer)
 
+const generateFeaturePostCard = () => {
 
+    const featuredPost = posts.find(post => post.featured)
+
+    if (!featuredPost) {
+        featuredPostContainer.innerHTML = "Post not found"
+        return
+    }
+
+    featuredPostContainer.innerHTML = `
+        <article>
+            <a 
+                href="post.html?id=${featuredPost.id}" 
+                class="featured-post-card"
+                style="background-image: url('${featuredPost.coverImage}')"
+            >
+                <span>${featuredPost.entryDate}</span>
+                <h2>${featuredPost.title}</h2>
+                <span>by ${featuredPost.author}</span>
+                <p>${featuredPost.excerpt}</p>
+            </a>
+
+        </article>
+    `
+}
+
+generateFeaturePostCard()
 
 
 const generateRecentPostsCards = () => {
     const recentPosts = posts
         .filter(post => !post.featured)
-        .sort((a,b) => new Date(b.entryDate) - new Date(a.entryDate))
+        .sort((a, b) => new Date(b.entryDate) - new Date(a.entryDate))
         .slice(0, 3)
-    
 
     const recentPostHtml = recentPosts.map(post => {
         return `<article>
-                    <a href="">
+                    <a href="post.html?id=${post.id}">
                         <img src="${post.coverImage}" alt="" />
-                       <span>${post.entryDate}</span>
-                       <h2>${post.title}</h2>
-                       <span>by ${post.author}</span>
-                       <p>${post.excerpt}</p>
+                        <span>${post.entryDate}</span>
+                        <h2>${post.title}</h2>
+                        <span>by ${post.author}</span>
+                        <p>${post.excerpt}</p>
                     </a>
                 </article>
                 `
-        
     }).join(' ')
 
     recentPostContainer.innerHTML = recentPostHtml
-
 }
 
 generateRecentPostsCards()
